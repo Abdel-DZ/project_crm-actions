9f594d9b9606ee4eb381af672280dfda73a7f92f
Abdel-DZ <abdellah.ahmed@student.nbi-handelsakademin.se>
2025-04-23 00:36:46 +0200

    Update changePassword.cs

--- E2ETesting-main/Steps/changePassword.cs
+++ E2ETesting-main/Steps/changePassword.cs
@@ -21,7 +21,7 @@ namespace E2ETesting.Steps
         public async Task Setup()
         {
             _playwright = await Playwright.CreateAsync();
-            _browser = await _playwright.Chromium.LaunchAsync(new() { Headless = false, SlowMo = 500 });
+            _browser = await _playwright.Chromium.LaunchAsync(new() { Headless = true, SlowMo = 500 });
             _context = await _browser.NewContextAsync();
             _page = await _context.NewPageAsync();
             _httpClient = new HttpClient();
